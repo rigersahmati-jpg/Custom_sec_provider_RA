79e0ba793cd6728eae830e1ba7510b0f212b554e
rigersahmati-jpg <rigers.ahmati@gmail.com>
2026-08-18 15:12:10 +0200

    Remove 'required' modifier from properties in ProviderSettings

--- src/CustomSecProvider.RA/Configuration/ProviderSettings.cs
+++ src/CustomSecProvider.RA/Configuration/ProviderSettings.cs
@@ -2,18 +2,18 @@ namespace CustomSecProvider.RA.Configuration;
 
 public sealed class ProviderSettings
 {
-    public required bool FailClosed { get; init; } = true;
-    public required int EntitlementCacheTtlSeconds { get; init; } = 60;
-    public required SeatPolicySettings SeatPolicy { get; init; } = new();
-    public required IncidentPolicySettings IncidentPolicy { get; init; } = new();
+    public bool FailClosed { get; init; } = true;
+    public int EntitlementCacheTtlSeconds { get; init; } = 60;
+    public SeatPolicySettings SeatPolicy { get; init; } = new();
+    public IncidentPolicySettings IncidentPolicy { get; init; } = new();
 }
 
 public sealed class SeatPolicySettings
 {
-    public required string OverageAction { get; init; } = "Deny";
+    public string OverageAction { get; init; } = "Deny";
 }
 
 public sealed class IncidentPolicySettings
 {
-    public required string Action { get; init; } = "ReadOnly";
+    public string Action { get; init; } = "ReadOnly";
 }
